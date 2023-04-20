e5836c2aef8f3c488c1c93acf70feb0c41a3b422
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-04-20 14:07:03 +0200

    Finished tables structure and created tables

--- app/sql/create_tables.sql
+++ app/sql/create_tables.sql
@@ -1,25 +1,69 @@
--- Athlete table that contains information about athletes
-CREATE TABLE IF NOT EXISTS athlete (
-    athleteID INT NOT NULL PRIMARY KEY,
-    fistName VARCHAR NOT NULL,
-    lastName VARCHAR NOT NULL,
-    age INT NOT NULL,
-    gender VARCHAR NOT NULL
+-- ================== Utils tables ==================
+
+-- Experience table that contains information about athlete experience level
+CREATE TABLE IF NOT EXISTS Experience (
+    experienceID INT NOT NULL,
+    experienceLevel VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (experienceID)
 );
 
 -- Exercise type table that contains information about types of exercise
-CREATE TABLE IF NOT EXISTS exerciseType (
-    exerciseID INT NOT NULL PRIMARY KEY,
-    exerciseTypeName VARCHAR NOT NULL
-)
+CREATE TABLE IF NOT EXISTS ExerciseType (
+    exerciseTypeID INT NOT NULL,
+    exerciseTypeName VARCHAR(100) NOT NULL,
 
--- Exercise table that contains information about single exercise for specific athlete
-CREATE TABLE IF NOT EXISTS exercise (
-    exerciseID INT NOT NULL PRIMARY KEY,
-)
+    PRIMARY KEY (exerciseTypeID)
+);
+
+-- Plan goal table that contains information about plan goals types
+CREATE TABLE IF NOT EXISTS PlanGoal (
+    planGoalID INT NOT NULL,
+    goalTypeName VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (planGoalID)
+);
+
+
+-- ================== Main tables ===================
+
+-- Athlete table that contains information about athletes
+CREATE TABLE IF NOT EXISTS Athlete (
+    athleteID INT NOT NULL,
+    fistName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    athleteWeight INT NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+    experienceID INT NOT NULL,
+
+    PRIMARY KEY (athleteID),
+    FOREIGN KEY (experienceID) REFERENCES Experience(experienceID)
+);
 
 -- Plan table that contains information about traning plan
-CREATE TABLE IF NOT EXISTS plan (
-    planID INT NOT NULL PRIMARY KEY,
-    planName VARCHAR NOT NULL
-)
+CREATE TABLE IF NOT EXISTS Plan (
+    planID INT NOT NULL,
+    planName VARCHAR(100) NOT NULL,
+    planGoalID INT NOT NULL,
+    planStart DATE NOT NULL,
+    planEnd DATE NOT NULL,
+
+    PRIMARY KEY (planID),
+    FOREIGN KEY (planGoalID) REFERENCES PlanGoal(planGoalID)
+);
+
+-- Exercise table that contains information about single exercise for specific athlete
+CREATE TABLE IF NOT EXISTS Exercise (
+    exerciseID INT NOT NULL,
+    athleteID INT NOT NULL,
+    planID INT NOT NULL,
+    exerciseTypeID INT NOT NULL,
+    setsCount INT NOT NULL,
+    repsPerSetCount INT NOT NULL,
+
+    PRIMARY KEY (exerciseID),
+    FOREIGN KEY (athleteID) REFERENCES Athlete(athleteID),
+    FOREIGN KEY (planID) REFERENCES Plan(planID),
+    FOREIGN KEY (exerciseTypeID) REFERENCES ExerciseType(exerciseTypeID)
+);
